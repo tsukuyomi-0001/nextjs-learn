cbc2138a4333ed3b8103967f9f1e9d94a8f7b2ba
tsukuyomi-0001 <shadowgenius033@gmail.com>
2025-08-24 16:31:59 +0530

    retry retry dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{childern}</div>
+            <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
         </div>
     )
 }
